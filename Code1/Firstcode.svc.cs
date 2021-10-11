9bb9b99619923353de26d9a9df481b616299445a
OkkyHaryanto <63486943+OkkyHaryanto@users.noreply.github.com>
2021-10-11 14:30:27 +0700

    Add files via upload

---
+++ Code1/Firstcode.svc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Code1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Firstcode" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Firstcode.svc or Firstcode.svc.cs at the Solution Explorer and start debugging.
+    public class Firstcode : IFirstcode
+    {
+        public string Message()
+        {
+            return "OKKY HARYANTO 20190140052";
+        }
+    }
+}
